680b019c0dc69b3c3b242aaf6b8db529f4c94da0
Abhilash Singh <abhilash4597@gmail.com>
2023-09-21 22:58:58 +0530

    update

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-const Max_Pokemon = 151;
+const Max_Pokemon = 2000;
 const listWrapper = document.querySelector('.list-wrapper');
 const searchInput = document.querySelector('#search-input');
 const numberFilter = document.querySelector('#number');
@@ -8,10 +8,10 @@ const notFoundMessage = document.querySelector('#not-found-message');
 let allPokemons = [];
 
 fetch(`https://pokeapi.co/api/v2/pokemon?limit=${Max_Pokemon}`)
-.then((response)=>
-    response.json())
+.then((response)=>{
+    return response.json();
+})
 .then((data)=>{
     allPokemons = data.results;
+    console.log(data.results)
 })
-
-console.log(allPokemons)
