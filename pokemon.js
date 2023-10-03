d48ce5762cb981a6d67897d8a5ae2ab9ea9bef09
Abhilash Singh <abhilash4597@gmail.com>
2023-10-04 00:02:32 +0530

    complete

--- pokemon.js
+++ pokemon.js
@@ -1,102 +1,98 @@
-const Max_Pokemon = 649;
-const listWrapper = document.querySelector('.list-wrapper');
-const searchInput = document.querySelector('#search-input');
-const numberFilter = document.querySelector('#number');
-const nameFilter = document.querySelector('#name');
-const notFoundMessage = document.querySelector('#not-found-message');
-const closeButton = document.querySelector('.search-close-icon');
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
 
 let allPokemons = [];
 
-fetch(`https://pokeapi.co/api/v2/pokemon?limit=${Max_Pokemon}`)
-.then((response)=>{
-    return response.json();
-})
-.then((data)=>{
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+  .then((response) => response.json())
+  .then((data) => {
     allPokemons = data.results;
     displayPokemons(allPokemons);
-})
+  });
 
-async function fetchPokimonBeforeRedirect(id){
-    try{
-        const[pokemons,pokemonSpecies]= await Promise.all([(fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)).then((response)=>{
-            return response.json();
-        }),
-    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((response)=>{
-            return response.json();
-        }),
-    
-    ])
+async function fetchPokemonDataBeforeRedirect(id) {
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+        res.json()
+      ),
+    ]);
     return true;
-    }
-    catch(error) {
-        console.error('Failed to fetch Pokemon data before redirect')
-    }
+  } catch (error) {
+    console.error("Failed to fetch Pokemon data before redirect");
+  }
 }
 
-function displayPokemons(pokemon){
-    listWrapper.innerHTML ='';
+function displayPokemons(pokemon) {
+  listWrapper.innerHTML = "";
 
-    pokemon.forEach((pokemon) => {
-        const pokemonID = pokemon.url.split('/')[6];
-        const listItem = document.createElement('div');
-        listItem.className = 'list-item';
-        listItem.innerHTML = `
+  pokemon.forEach((pokemon) => {
+    const pokemonID = pokemon.url.split("/")[6];
+    const listItem = document.createElement("div");
+    listItem.className = "list-item";
+    listItem.innerHTML = `
         <div class="number-wrap">
             <p class="caption-fonts">#${pokemonID}</p>
         </div>
         <div class="img-wrap">
-            <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}">
+            <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
         </div>
         <div class="name-wrap">
             <p class="body3-fonts">#${pokemon.name}</p>
         </div>
-        `
-        
-        listItem.addEventListener('click',async ()=>{
-            const success = await fetchPokimonBeforeRedirect(pokemonID);
+    `;
 
-            if(success){
-                window.location.href = `./detail.html?id=${pokemonID}`;
-            }
-        })
-
-        listWrapper.appendChild(listItem);
+    listItem.addEventListener("click", async () => {
+      const success = await fetchPokemonDataBeforeRedirect(pokemonID);
+      if (success) {
+        window.location.href = `./detail.html?id=${pokemonID}`;
+      }
     });
+
+    listWrapper.appendChild(listItem);
+  });
 }
 
-searchInput.addEventListener('keyup',handleSearch);
+searchInput.addEventListener("keyup", handleSearch);
 
-function handleSearch(){
-    const searchItem = searchInput.value.toLowerCase();
-    let filteredPokemon;
+function handleSearch() {
+  const searchTerm = searchInput.value.toLowerCase();
+  let filteredPokemons;
 
-    if(numberFilter.checked){
-        filteredPokemon = allPokemons.filter((pokemon)=>{
-            const pokemonID = pokemon.url.split('/')[6];
-            return pokemonID.startsWith(searchItem)
-        })
-    }else if(nameFilter.checked){
-        filteredPokemon = allPokemons.filter((pokemon)=>{
-            pokemon.name.toLowerCase().startsWith(searchItem);
-        })
-    }else {
-        filteredPokemon = allPokemons;
-    }
+  if (numberFilter.checked) {
+    filteredPokemons = allPokemons.filter((pokemon) => {
+      const pokemonID = pokemon.url.split("/")[6];
+      return pokemonID.startsWith(searchTerm);
+    });
+  } else if (nameFilter.checked) {
+    filteredPokemons = allPokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().startsWith(searchTerm)
+    );
+  } else {
+    filteredPokemons = allPokemons;
+  }
 
-    displayPokemons(filteredPokemon);
+  displayPokemons(filteredPokemons);
 
-    if(filteredPokemon.length===0){
-        notFoundMessage.style.display = 'block';
-    }else {
-        notFoundMessage.style.display = 'none';
-    }
+  if (filteredPokemons.length === 0) {
+    notFoundMessage.style.display = "block";
+  } else {
+    notFoundMessage.style.display = "none";
+  }
 }
 
-closeButton.addEventListener('click',clearSearch);
+const closeButton = document.querySelector(".search-close-icon");
+closeButton.addEventListener("click", clearSearch);
 
-function clearSearch(){
-    searchInput.value = '';
-    displayPokemons(allPokemons);
-    notFoundMessage.style.display = 'none';
+function clearSearch() {
+  searchInput.value = "";
+  displayPokemons(allPokemons);
+  notFoundMessage.style.display = "none";
 }
